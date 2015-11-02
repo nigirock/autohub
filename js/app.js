cf3e29c15fa3372d1335ed067fd58e6b397fdcaf
nigirock <nigirock124@gmail.com>
2015-11-02 14:56:57 +0300

    popover create

--- js/app.js
+++ js/app.js
@@ -2,6 +2,8 @@ var app = angular.module("APP",[]);
 app.controller("mainCtrl",function($scope,$http){
     $http.get("data.json").success(function(data){
         $scope.cars = data.cars;
-        console.log($scope.page);
+        $scope.carsModels = Object.keys(data.modelsCars[0]);
+
+        console.log();
     });
 });
--- js/directives.js
+++ js/directives.js
@@ -9,6 +9,35 @@ app.directive("ngHeader",function(){
 app.directive("ngLeftMenu",function(){
     return{
         link: function(scope,element,attributes){
+            scope.visiblePopupMark = false;
+            scope.visiblePopupPrice = false;
+            scope.defaultLeftMenu =["Цена до 100 000$","Любая марка","Любая модель","Не старше 2008 года","Любой тип топлива","Объем не менее 1.0","Любая коробка передач","пробег до 200 000 км"];
+            scope.addPopup = function(index){
+                switch (index){
+                    case(index = 0):
+                        scope.visiblePopupPrice = true;
+                        break;
+                    case (index = 1):
+                        scope.visiblePopupMark = true;
+                        break;
+                }
+                console.log(scope.visiblePopupPrice);
+            };
+            scope.addPopupLeave = function(index){
+                switch (index){
+                    case(index = 0):
+                        scope.visiblePopupPrice = false;
+                        break;
+                    case (index = 1):
+                        scope.visiblePopupMark = false;
+                        break;
+                }
+                console.log(scope.visiblePopupPrice);
+            };
+
+            scope.addCarSearch = function(nameCar){
+                scope.defaultLeftMenu[1] = nameCar;
+            };
         },
         restrict: "A",
         templateUrl: "left_menu.html"
@@ -30,5 +59,14 @@ app.directive("ngFooter",function(){
         },
         restrict: "A",
         templateUrl: "footer.html"
+    }
+});
+app.directive("ngPopover",function(){
+    return{
+        link: function(scope,element,attributes){
+
+        },
+        restrict: "A",
+        templateUrl: "popover.html"
     }
 });
